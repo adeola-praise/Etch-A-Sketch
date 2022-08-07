3607f1c933d65cd72fd9cb493b10f2c6c3f7cba7
adeola-praise <68433446+adeola-praise@users.noreply.github.com>
2022-08-07 15:17:24 +0100

    Set Mouse Down Effect

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 // Get the grid container
 let gridContainer = document.querySelector('.grid-container');
 
+function createElement(){
+}
+
 for(let i=0; i <256; i++){
     // Create the square grids
     let squareDiv = document.createElement('div');
@@ -10,5 +13,18 @@ for(let i=0; i <256; i++){
 
     // Append the square div to the grid container
     gridContainer.appendChild(squareDiv);
-    console.log('oya');
 }
+
+let squares = document.getElementsByClassName('square');
+
+// Create an array from the squares
+let squaresArray = Array.from(squares);
+
+// Loop through the square array 
+squaresArray.forEach(function(item){
+    // Change background color on mouse down
+    item.onmousedown = function(){
+        item.style.backgroundColor = 'red';
+    }
+})
+
