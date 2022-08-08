deabc0cf366239c43f913eb76a21bc0bb96ec926
adeola-praise <68433446+adeola-praise@users.noreply.github.com>
2022-08-08 18:32:18 +0100

    Design Color Picker PopUp

--- scripts.js
+++ scripts.js
@@ -55,20 +55,109 @@ function changeColor() {
     // Create an array from the squares
     let squaresArray = Array.from(squares);
 
+    // Get the random color generator button
+    let randomBtn = document.getElementById('randomBtn');
+
     // Loop through the square array 
     squaresArray.forEach(function (item){
         // Change background color on mouse down
-        item.onmouseover = function(){
-            item.style.backgroundColor = 'red';
+        item.onmousedown = function(){
+            let x = Math.floor(Math.random() * 256);
+            let y = Math.floor(Math.random() * 256);
+            let z = Math.floor(Math.random() * 256);
+            
+            let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+            item.style.backgroundColor = bgColor;
         }
     });
 }
 
-function resizeGrid(item) {
-     // Give the divs their width
-     item.style.width = 384/Math.sqrt(area);
+// Get the color picker button
+let revealColor = document.querySelector('.pickColor');
 
-     // Give the divs their height
-     item.style.height = 468/Math.sqrt(area);
+// Get the color picker
+let hiddenPicker = document.querySelector('.colorPicker');
+
+// Get the okay button to confirm picked color
+let okBtn = document.querySelector('#ok') 
+
+// Get the cancel button to ignore picked color
+let cancelBtn = document.querySelector('#cancel');
+
+function revealRGB() {
+    hiddenPicker.style.display = 'flex';
+    okBtn.onclick = function(){
+        hiddenPicker.style.display = 'none';
+    }
+    cancelBtn.onclick = function(){
+        hiddenPicker.style.display = 'none';
+    }
 }
+
+revealColor.addEventListener('click', revealRGB);
+
+// SET UP THE COLOR PICKER //
+let r = document.querySelector('#r'),
+    g = document.querySelector('#g'),
+    b = document.querySelector('#b'),
+    r_out = document.querySelector('#r_out'),
+    g_out = document.querySelector('#g_out'),
+    b_out = document.querySelector('#b_out'),
+    hex_out = document.querySelector('#hex');
+    hex_box = document.querySelector('#hex-color');
+
+function setColor(){
+    let r_hex = parseInt(r.value, 10).toString(16),
+        g_hex = parseInt(g.value, 10).toString(16),
+        b_hex = parseInt(b.value, 10).toString(16),
+        hex = "#" + pad(r_hex) + pad(g_hex) + pad(b_hex);
+        hex_box.style.backgroundColor = hex; 
+    hex_out.value = hex;
+  }
+  
+  function pad(n){
+    return (n.length<2) ? "0"+n : n;
+  }
+  
+  r.addEventListener('change', function() {
+    setColor();
+    r_out.value = r.value;
+  }, false);
+  
+  r.addEventListener('input', function() {
+    setColor();
+    r_out.value = r.value;
+  }, false);
+  
+  g.addEventListener('change', function() {
+    setColor();
+    g_out.value = g.value;
+  }, false);
+  
+  g.addEventListener('input', function() {
+    setColor();
+    g_out.value = g.value;
+  }, false);
+  
+  b.addEventListener('change', function() {
+    setColor();
+    b_out.value = b.value;
+  }, false);
+  
+  b.addEventListener('input', function() {
+    setColor();
+    b_out.value = b.value;
+  }, false);
+
+// Create a function that would generate random rgb colors
+// so I need the entire rgb colors and randomly generate them
+
+function random_bg_color() {
+    
+    
+  
+    //document.body.style.background = bgColor;
+}
+
+random_bg_color();
 
